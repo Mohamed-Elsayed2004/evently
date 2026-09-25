3665422a9003d9a6013389def4636a933bc94dd3
Mohamed-Elsayed2004 <mohamedelsayed20045@gmail.com>
2026-09-25 17:13:30 +0300

    fix: remove unused import for flutter_svg in login_screen.dart

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -11,7 +11,7 @@ import 'package:evently/utils/app_text_style.dart';
 import 'package:evently/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/svg.dart';
+
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
